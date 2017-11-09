3173c0262351ac88b69c0eb0244a179d44a0e088
Bruno Garcia <bruno@brunogarcia.com>
2017-11-09 22:46:51 +0100

    all projects in solution and first Extensions test

---
+++ test/Messaging.DependencyInjection.Serialization.Xml.Tests/XmlMessagingBuilderExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Serialization;
+using Serialization.Xml;
+using Xunit;
+
+namespace Messaging.DependencyInjection.Serialization.Xml.Tests
+{
+    public class XmlMessagingBuilderExtensionsTests
+    {
+        class Fixture
+        {
+            public ServiceCollection ServiceCollection { get; set; } = new ServiceCollection();
+            public MessagingBuilder GetBuilder() => new MessagingBuilder(ServiceCollection);
+        }
+
+        private readonly Fixture _fixture = new Fixture();
+
+        [Fact]
+        public void AddXml_RegistersXmlSerializer()
+        {
+            var builder = _fixture.GetBuilder();
+            builder.AddXml();
+
+            var descriptor = _fixture.ServiceCollection.FirstOrDefault(d => d.ServiceType == typeof(ISerializer));
+            Assert.NotNull(descriptor);
+
+            Assert.Equal(ServiceLifetime.Singleton, descriptor.Lifetime);
+            Assert.Equal(typeof(XmlSerializer), descriptor.ImplementationType);
+        }
+    }
+}
