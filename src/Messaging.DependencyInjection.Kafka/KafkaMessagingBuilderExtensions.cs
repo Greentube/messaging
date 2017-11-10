724678aef083d6bd0a9a81095aa76b55af611bc2
Bruno Garcia <bruno.garcia@greentube.com>
2017-11-10 17:29:38 +0100

    Kafka DI extensions tested

--- src/Messaging.DependencyInjection.Kafka/KafkaMessagingBuilderExtensions.cs
+++ src/Messaging.DependencyInjection.Kafka/KafkaMessagingBuilderExtensions.cs
@@ -1,24 +1,85 @@
 ﻿using System;
 using Messaging.DependencyInjection;
 using Messaging.Kafka;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class KafkaMessagingBuilderExtensions
     {
-        public static MessagingBuilder AddKafka(this MessagingBuilder builder)
-        {
-            builder.AddRawMessagePublisher<KafkaRawMessagePublisher>();
-            builder.AddRawMessageHandlerSubscriber<KafkaRawMessageHandlerSubscriber>();
-            builder.Services.AddSingleton(c => c.GetRequiredService<IOptions<KafkaOptions>>().Value);
-            return builder;
-        }
-
+        /// <summary>
+        /// Adds both Kafka publisher and subscriber
+        /// </summary>
+        /// <param name="builder">Messaging builder</param>
+        /// <param name="actionSetup">Setup Kafka options</param>
+        /// <returns></returns>
         public static MessagingBuilder AddKafka(this MessagingBuilder builder, Action<KafkaOptions> actionSetup)
         {
             builder.Services.Configure(actionSetup);
             return builder.AddKafka();
+        }
+
+        /// <summary>
+        /// Adds both Kafka publisher and subscriber
+        /// </summary>
+        /// <param name="builder">Messaging builder</param>
+        /// <returns></returns>
+        public static MessagingBuilder AddKafka(this MessagingBuilder builder)
+        {
+            builder.AddKafkaPublisher();
+            builder.AddKafkaSubscriber();
+            return builder;
+        }
+
+        /// <summary>
+        /// Register Kafka raw publisher service
+        /// </summary>
+        /// <param name="builder">Messaging builder</param>
+        /// <param name="actionSetup">Setup Kafka options</param>
+        /// <returns></returns>
+        public static MessagingBuilder AddKafkaPublisher(this MessagingBuilder builder, Action<KafkaOptions> actionSetup)
+        {
+            builder.Services.Configure(actionSetup);
+            builder.AddKafkaPublisher();
+            return builder;
+        }
+
+        /// <summary>
+        /// Register Kafka raw subscriber service
+        /// </summary>
+        /// <param name="builder">Messaging builder</param>
+        /// <param name="actionSetup">Setup Kafka options</param>
+        /// <returns></returns>
+        public static MessagingBuilder AddKafkaSubscriber(this MessagingBuilder builder, Action<KafkaOptions> actionSetup)
+        {
+            builder.Services.Configure(actionSetup);
+            builder.AddKafkaSubscriber();
+            return builder;
+        }
+
+        /// <summary>
+        /// Register Kafka raw publisher service
+        /// </summary>
+        /// <param name="builder">Messaging builder</param>
+        /// <returns></returns>
+        public static MessagingBuilder AddKafkaPublisher(this MessagingBuilder builder)
+        {
+            builder.Services.TryAddSingleton(c => c.GetRequiredService<IOptions<KafkaOptions>>().Value);
+            builder.AddRawMessagePublisher<KafkaRawMessagePublisher>();
+            return builder;
+        }
+
+        /// <summary>
+        /// Register Kafka raw subscriber service
+        /// </summary>
+        /// <param name="builder">Messaging builder</param>
+        /// <returns></returns>
+        public static MessagingBuilder AddKafkaSubscriber(this MessagingBuilder builder)
+        {
+            builder.Services.TryAddSingleton(c => c.GetRequiredService<IOptions<KafkaOptions>>().Value);
+            builder.AddRawMessageHandlerSubscriber<KafkaRawMessageHandlerSubscriber>();
+            return builder;
         }
     }
 }
---
+++ test/Messaging.DependencyInjection.Kafka.Tests/KafkaMessagingBuilderExtensionsTests.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Linq;
+using Messaging.Kafka;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace Messaging.DependencyInjection.Kafka.Tests
+{
+    public class KafkaMessagingBuilderExtensionsTests
+    {
+        class Fixture
+        {
+            public ServiceCollection ServiceCollection { get; } = new ServiceCollection();
+            public MessagingBuilder GetBuilder() => new MessagingBuilder(ServiceCollection);
+        }
+
+        private readonly Fixture _fixture = new Fixture();
+
+        [Fact]
+        public void AddKafka_RegistersPublisherSubscriberAndOptions()
+        {
+            // Arrange
+            var builder = _fixture.GetBuilder();
+
+            // Act
+            builder.AddKafka();
+
+            // Assert
+            AssertPublisher();
+            AssertSubscriber();
+            AssertOptions();
+        }
+
+        [Fact]
+        public void AddKafka_Action_RegistersPublisherSubscriberOptionsAndSetupAction()
+        {
+            // Arrange
+            var builder = _fixture.GetBuilder();
+            // ReSharper disable once ConvertToLocalFunction - Reference is needed for comparison
+            Action<KafkaOptions> configAction = _ => { };
+
+            // Act
+            builder.AddKafka(configAction);
+
+            // Assert
+            AssertOptionSetup(configAction);
+            AssertPublisher();
+            AssertSubscriber();
+            AssertOptions();
+        }
+
+        [Fact]
+        public void AddKafkaPublisher_RegisterOnlyPublisher()
+        {
+            // Arrange
+            var builder = _fixture.GetBuilder();
+
+            // Act
+            builder.AddKafkaPublisher();
+
+            // Assert
+            AssertPublisher();
+            AssertOptions();
+            Assert.False(_fixture.ServiceCollection.Any(d => d.ServiceType == typeof(IRawMessageHandlerSubscriber)));
+        }
+
+        [Fact]
+        public void AddKafkaPublisher_Action_RegisterOnlyPublisherAndSetupAction()
+        {
+            // Arrange
+            var builder = _fixture.GetBuilder();
+            // ReSharper disable once ConvertToLocalFunction - Reference is needed for comparison
+            Action<KafkaOptions> configAction = _ => { };
+
+            // Act
+            builder.AddKafkaPublisher(configAction);
+
+            // Assert
+            AssertOptionSetup(configAction);
+            AssertPublisher();
+            AssertOptions();
+            Assert.False(_fixture.ServiceCollection.Any(d => d.ServiceType == typeof(IRawMessageHandlerSubscriber)));
+        }
+
+        [Fact]
+        public void AddKafkaSubscriber_RegisterOnlySubscriber()
+        {
+            // Arrange
+            var builder = _fixture.GetBuilder();
+
+            // Act
+            builder.AddKafkaSubscriber();
+
+            // Assert
+            AssertOptions();
+            AssertSubscriber();
+            Assert.False(_fixture.ServiceCollection.Any(d => d.ServiceType == typeof(IRawMessagePublisher)));
+        }
+
+        [Fact]
+        public void AddKafkaSubscriber_Action_RegisterOnlySubscriberAndSetupAction()
+        {
+            // Arrange
+            var builder = _fixture.GetBuilder();
+            // ReSharper disable once ConvertToLocalFunction - Reference is needed for comparison
+            Action<KafkaOptions> configAction = _ => { };
+
+            // Act
+            builder.AddKafkaSubscriber(configAction);
+
+            // Assert
+            AssertOptionSetup(configAction);
+            AssertSubscriber();
+            AssertOptions();
+            Assert.False(_fixture.ServiceCollection.Any(d => d.ServiceType == typeof(IRawMessagePublisher)));
+        }
+
+        private void AssertSubscriber()
+        {
+            var subscriber =
+                _fixture.ServiceCollection.FirstOrDefault(d => d.ServiceType == typeof(IRawMessageHandlerSubscriber));
+            Assert.NotNull(subscriber);
+
+            Assert.Equal(ServiceLifetime.Singleton, subscriber.Lifetime);
+            Assert.Equal(typeof(KafkaRawMessageHandlerSubscriber), subscriber.ImplementationType);
+        }
+
+        private void AssertPublisher()
+        {
+            var publisher =
+                _fixture.ServiceCollection.FirstOrDefault(d => d.ServiceType == typeof(IRawMessagePublisher));
+            Assert.NotNull(publisher);
+
+            Assert.Equal(ServiceLifetime.Singleton, publisher.Lifetime);
+            Assert.Equal(typeof(KafkaRawMessagePublisher), publisher.ImplementationType);
+        }
+
+        private void AssertOptions()
+        {
+            var options =
+                _fixture.ServiceCollection.FirstOrDefault(d => d.ServiceType == typeof(KafkaOptions));
+            Assert.NotNull(options);
+        }
+
+        private void AssertOptionSetup(Action<KafkaOptions> configAction)
+        {
+            var optionsConfiguration =
+                _fixture.ServiceCollection.FirstOrDefault(d => d.ServiceType == typeof(IConfigureOptions<KafkaOptions>));
+            Assert.Same(configAction,
+                ((ConfigureNamedOptions<KafkaOptions>) optionsConfiguration.ImplementationInstance).Action);
+        }
+    }
+}
